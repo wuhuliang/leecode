1b7759c4d6892514b5943804311452908b6ecbc3
wuhuliang <782170284@qq.com>
2017-09-13 11:27:16 +0800

    9月7日

--- HammimgDistance.py
+++ HammimgDistance.py
@@ -4,6 +4,7 @@
 '''
     demo1:定义一个返回两个整数和的函数
 '''
+
 # def add(a, b):
 #     return a + b
 #
@@ -34,19 +35,60 @@
 demo3：输入三个数，输出最大数
 '''
 
-def max_w(a,b,c):
-    MAX = a
-    if (b > a):
-        MAX = b
-        if (c > MAX):
-            MAX = c
-    return MAX
+# def max_w(a,b,c):
+#     MAX = a
+#     if (b > a):
+#         MAX = b
+#         if (c > MAX):
+#             MAX = c
+#     return MAX
+#
+#
+# x = int(raw_input("请输入第一个数："))
+# y = int(raw_input("请输入第二个数："))
+# z = int(raw_input("请输入第三个数: "))
+#
+#
+# print ("%d,%d,%d,三个数字中,最大的是 %d:" %(x,y,z,max_w(x,y,z)))
+
+'''
+    demo4:这是一个函数版模拟用户注册登录
+'''
+
+# 定义保存用户名和密码的字典
+userInfo = {}
+
+# 是否登录的标志
+islog = False
+
+# 注册方法
+def register():
+    uname = raw_input("添加用户名: ")
+    upass = raw_input("添加密码: ")
+    userInfo[str(uname)] = upass
+    print("注册成功")
+
+# 登录方法
+def login():
+    for i in range(3):
+        global islog
+        if(islog):
+            break
+        username_input = raw_input('请输入用户名:')
+        userpass_input = raw_input("请输入密码:")
+        login_count = 2-i  # 判断还能输入多少次。
+        for key in userInfo.keys():
+            if(str(username_input) == key and userInfo[key]== userpass_input):
+                print("欢迎 %s 登录"%key)
+                islog = True
+                break
+            else:
+                # 最后一次是 0 不再提示
+                if login_count != 0:
+                    print("用户名或密码错误，还可以输入 %d 次"%login_count)
+        if(i==2):
+            print("登录超过 3 次，半个小时后再登录")
 
 
-x = int(raw_input("请输入第一个数："))
-y = int(raw_input("请输入第二个数："))
-z = int(raw_input("请输入第三个数: "))
-
-
-print ("%d,%d,%d,三个数字中,最大的是 %d:" %(x,y,z,max_w(x,y,z)))
-
+register()
+login()
