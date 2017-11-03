a8bc1a122cba223762503d26ee27cf96bef0d73b
wuhuliang <782170284@qq.com>
2017-11-03 10:34:46 +0800

    加入重构

--- storage.py
+++ storage.py
@@ -23,14 +23,51 @@ import json
 """
 保存和读取用户生成的数据
 """
-filename = 'username.json'
-try:
-    with open(filename) as f:
-        uname = json.load(f)
-except IOError:
+# filename = 'username.json'
+# try:
+#     with open(filename) as f:
+#         uname = json.load(f)
+# except IOError:
+#     username = raw_input("Please input your name:")
+#     with open(filename,'w') as f:
+#         json.dump(username,f)
+#         print ("Information has been storaged.")
+# else:
+#     print ("Hello, " + uname)
+
+"""
+重构
+"""
+def get_stored_username():
+    filename = 'username.json'
+    try:
+        with open(filename) as f:
+            uname = json.load(f)
+    except IOError:
+        return None
+    else:
+        return uname
+
+def get_new_username():
+    filename = 'username.json'
     username = raw_input("Please input your name:")
-    with open(filename,'w') as f:
-        json.dump(username,f)
+    with open(filename, 'w') as f:
+        json.dump(username, f)
         print ("Information has been storaged.")
-else:
-    print ("Hello, " + uname)
+    return username
+
+def greet_user():
+    username = get_stored_username()
+    if username:
+        print ("Username is " + username + ". IF the username correct?(Y/N)")
+        aswer = raw_input()
+        if aswer == "Y":
+            print ("Hello, " + username)
+        else:
+            get_new_username()
+    else:
+        get_new_username()
+
+
+greet_user()
+
